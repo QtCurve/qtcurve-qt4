8889ee355af6c0f0e315f75ef0afde130d7dcde9
Yichao Yu <yyc1992@gmail.com>
2013-10-08 10:47:14 -0400

    [qt4] try solving #7, cherry-picked from QtCurve/qtcurve

--- qt4/style/qtcurve.cpp
+++ qt4/style/qtcurve.cpp
@@ -754,6 +754,13 @@ inline int numButtons(EScrollbar type)
     }
 }
 
+// TODO make this better.
+static inline int
+sizeNumButtons(EScrollbar type)
+{
+    return qMax(numButtons(type), 2);
+}
+
 static inline void drawRect(QPainter *p, const QRect &r)
 {
     p->drawRect(r.x(), r.y(), r.width()-1, r.height()-1);
@@ -9696,9 +9703,9 @@ QSize Style::sizeFromContents(ContentsType type, const QStyleOption *option, con
                     scrollBarSliderMinimum(pixelMetric(PM_ScrollBarSliderMin, option, widget));
 
                 if (scrollBar->orientation == Qt::Horizontal)
-                    newSize = QSize(scrollBarExtent * numButtons(opts.scrollbarType) + scrollBarSliderMinimum, scrollBarExtent);
+                    newSize = QSize(scrollBarExtent * sizeNumButtons(opts.scrollbarType) + scrollBarSliderMinimum, scrollBarExtent);
                 else
-                    newSize = QSize(scrollBarExtent, scrollBarExtent * numButtons(opts.scrollbarType) + scrollBarSliderMinimum);
+                    newSize = QSize(scrollBarExtent, scrollBarExtent * sizeNumButtons(opts.scrollbarType) + scrollBarSliderMinimum);
             }
             break;
         case CT_LineEdit:
